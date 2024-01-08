efdd272439a4e8a626e6df966699967e9f6a5616
AugustoAleman <a01660702@tec.mx>
2024-01-08 16:39:03 -0600

    Added Eureka Files

---
+++ infra/eureka/src/test/java/com/itesm/prueba/erueka/EruekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itesm.prueba.erueka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EruekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
